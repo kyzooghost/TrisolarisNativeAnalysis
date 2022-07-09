932a33e09ac12ae7e7222d76c96cc927b123b1d5
kyzooghost <73516204+kyzooghost@users.noreply.github.com>
2022-07-09 12:44:01 +0000

    Initial commit

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+import { BigNumber } from "ethers"
+
+export const AURORA_URL = "https://mainnet.aurora.dev/";
+export const ZERO = BigNumber.from("0")
+export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
